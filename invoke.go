96e50bf21c90c2ffd99901f82fce313d7ccd5263
Manitra Ranaivoharison <manitra.ranaivoharison@converteo.com>
2023-07-25 10:33:24 +0200

    update of invoke.go and add github workflow

--- invoke.go
+++ invoke.go
@@ -6,17 +6,9 @@ import (
         "os"
         "os/exec"
 )
-
-type HandlerData struct {
-        data string
-        //whatever
-    }
-
-    
-func (h *HandlerData) handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
         log.Print("helloworld: received a request")
-cmd := exec.Command("dbt",h.data)
-        log.Print(h.data)
+cmd := exec.Command("dbt", "script.sh")
         cmd.Stdout = os.Stdout
  cmd.Stderr = os.Stderr
  err := cmd.Run()
@@ -24,16 +16,9 @@ cmd := exec.Command("dbt",h.data)
   log.Fatalf("cmd.Run() failed with %s\n", err)
  }
 }
-
 func main() {
         log.Print("helloworld: starting server...")
-        //Create a data struct
-        // add data to struct
-        h := &HandlerData{...}
-
-http.HandleFunc("/", handler())
-
-
+http.HandleFunc("/", handler)
 port := os.Getenv("PORT")
         if port == "" {
                 port = "8080"

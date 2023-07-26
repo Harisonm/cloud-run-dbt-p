baea834ab148bbf380c8ee9a84aa6d62e984f0c3
Manitra Ranaivoharison <manitra.ranaivoharison@converteo.com>
2023-07-26 22:07:34 +0200

    change project and Dockerfile

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
+from bigquery_public_data import app,db
 import os
 
-from flask import Flask
+with app.app_context():
+    db.create_all()
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
-
-if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
-    
+if __name__=='__main__':
+    app.run(debug=True,host="0.0.0.0",port=int(os.envion.get("PORT",8080)))

f3b8423b8aa560226ccfa93d721f66ee93cd0268
Manitra Ranaivoharison <manitra.ranaivoharison@converteo.com>
2023-07-27 08:38:43 +0200

    update of main

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ client = logging.Client()
 
 with app.app_context():
     db.create_all()
+    
+@app.route("/")
+def hello_world():
+    """Example Hello World route."""
+    name = os.environ.get("NAME", "World")
+    return f"Hello {name}!"
 
-if __name__=='__main__':
-
+@app.route("/dbt")
+def hello_world():
     logging.info(sys.argv, len(sys.argv))
     client.setup_logging()
     process = subprocess.run(['dbt', sys.argv], 
                          stdout=subprocess.PIPE, 
                          universal_newlines=True)
     process
-    
+
+if __name__=='__main__':
     app.run(debug=True,host="0.0.0.0",port=int(os.envion.get("PORT",8080)))
     

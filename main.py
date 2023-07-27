41de8c6c5fed6686cb703e27552428e4f0dce51e
Manitra Ranaivoharison <manitra.ranaivoharison@converteo.com>
2023-07-27 08:55:31 +0200

    fix main

--- main.py
+++ main.py
@@ -3,28 +3,21 @@ import os
 import sys
 from google.cloud import logging
 import subprocess
-
+from flask import Flask
 # Instantiates a client
 client = logging.Client()
 
 with app.app_context():
     db.create_all()
     
+app = Flask(__name__)
+
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
     name = os.environ.get("NAME", "World")
     return f"Hello {name}!"
 
-# @app.route("/dbt")
-# def hello_world():
-#     logging.info(sys.argv, len(sys.argv))
-#     client.setup_logging()
-#     process = subprocess.run(['dbt', sys.argv], 
-#                          stdout=subprocess.PIPE, 
-#                          universal_newlines=True)
-#     process
-
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
     
